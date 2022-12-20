c2d06a4c7986e2de3d4592ad67a306258bb9f300
Cam Frederickson <jofr1815@colorado.edu>
2022-12-20 15:30:15 -0700

    Copied files from Github Classroom repository

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+);
+
+app.get('/', (req, res) =>{
+    res.redirect('/home');
+});
+
+app.get('/home', (req, res) =>{
+    if (req.query.drinkName != "" && req.query.drinkName != undefined){
+        axios({
+            url: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${req.query.drinkName}`,
+            method: 'GET',
+            headers: {
+                'Accept-Encoding': 'application/json'
+            }
+        })
+        .then(results =>{
+            if(results.data.drinks){
+                res.render('pages/main', {
+                    data: results.data
+                });
+            } else {
+                res.render('pages/main', {
+                    error: true,
+                    message: "No drinks found"
+                });
+            }
+        })
+        .catch(error =>{
+            res.render('pages/main', 
+            {
+                error: true,
+                message: error
+            });
+        });
+    } else {
+        res.render('pages/main');
+    }
+});
+
+app.get('/reviews', (req, res) => {
+    res.render('pages/main', 
+    {
+        error: true,
+        message: 'Link Unavailable'
+    });
+});
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
